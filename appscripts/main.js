429c35c49ef4aa88639eefc4cce4d1d2d8d819e4
cassandra <sf99cass@gmail.com>
2025-07-11 22:04:57 +0800

    upload 1

---
+++ appscripts/main.js
@@ -0,0 +1,12 @@
+// main.js
+
+console.log(`yo`);
+
+var playButt = document.getElementById('playButt');
+playButt.addEventListener('click', function(ev){
+	window.open('project/index.html', '_blank');
+
+	// location.href='https://google.com';
+});
+
+
